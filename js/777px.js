fe17af6b8083a3e1839d7a5ec0c9d4bae238fcce
Matt Keas <matthiasak@gmail.com>
2015-02-26 15:01:01 -0600

    scrollarama

--- js/777px.js
+++ js/777px.js
@@ -18,14 +18,14 @@
             React.render(coffee, this.container)
             c.fetch()
 
-            var pusher = new Pusher('9e88d8c8fcbf4c114c30');
-            var channel = pusher.subscribe('seven-live');
-            channel.bind('photoCreated', function(data) {
-                console.log('attempting to add:', data)
-                // var newPhoto = new Backbone.Photo({id: data.id})
-                // newPhoto.fetch().then(function(){
-                c.fetch();
-            });
+            // var pusher = new Pusher('9e88d8c8fcbf4c114c30');
+            // var channel = pusher.subscribe('seven-live');
+            // channel.bind('photoCreated', function(data) {
+            //     console.log('attempting to add:', data)
+            //     // var newPhoto = new Backbone.Photo({id: data.id})
+            //     // newPhoto.fetch().then(function(){
+            //     c.fetch();
+            // });
         }
     })
 
@@ -46,9 +46,50 @@
         }
     })
 
-    // var PhotoView = React.createClass({
-    //     render: function
-    // })
+    var PhotoView = React.createClass({
+        className: "Photo",
+        getDefaultProps: function(){
+            return {}
+        },
+        getInitialState: function(){
+            return {
+                loaded: false,
+                inViewport: false
+            }
+        },
+        componentWillMount: function(){
+            var self = this;
+            this.onscroll = function(e, force){
+                var inViewport = self._getScrollTop(force)
+                if(inViewport){
+                    self.refs.img.getDOMNode().classList.add("loaded")
+                } else {
+                    self.refs.img.getDOMNode().classList.remove("loaded")
+                }
+            }
+            window.addEventListener('scroll', this.onscroll)
+        },
+        componentWillUnmount: function(){
+            window.removeEventListener('scroll', this.onscroll)
+        },
+        _getScrollTop: function(loaded){
+            var a = this.refs.img.getDOMNode(),
+                _y = a.offsetTop,
+                b = a.offsetHeight - 100,
+                h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            return (loaded || this.state.loaded) && ((_y + b > window.scrollY) && (_y + 100 < window.scrollY + h))
+        },
+        _loaded: function(){
+            this.setState({ loaded: true })
+            this.onscroll(null, true);
+        },
+        render: function(){
+            var model = this.props.model;
+            return d("div", [
+                d("img.dynamic@img", {src: model && model.get("large_photo_url"), onLoad: this._loaded} ),
+            ])
+        }
+    })
 
     var CoffeeGoggles = React.createClass({
         getInitialState: function(){
@@ -64,14 +105,22 @@
             })
         },
         render: function(){
+            Pace.restart();
+            var arr = this.props.collection.models,
+                arr2 = [],
+                i = 0;
+            // duplicate tenfold for scrolling
+            while(i < 10){
+                arr2.push.apply(arr2, arr)
+                i++;
+            }
+
             return d('div', [
                 d('div.grid.grid-3-600.grid-2-400',
-                    this.props.collection.models.map((function(model){
-                        return d("span", {key: model.id}, [
-                            d("img", {src: model.get("thumb_photo_url")} )
-                        ])
+                    arr2.map((function(model){
+                        return d(PhotoView, { model: model, key: model.id+Math.random() })
                     }).bind(this))
-                )
+                ),
             ])
         }
     })
